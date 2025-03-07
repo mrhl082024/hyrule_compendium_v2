6ca4c361157086ba6c13a6ed8df585d7d118ba03
mrhl082024 <mrhl082024@gmail.com>
2025-03-07 09:51:26 +0100

    ADD App

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 import Homepage from "./components/Homepage.jsx";
 import { useEffect, useState } from "react";
+import Navigationbar from "./components/Navigationbar.jsx";
 
 function App() {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState();
+  const [cache, setCache] = useState();
+  const [entryId, setEntryId] = useState();
+
+  const apiCategory = [
+    "Entry",
+    "Creatures",
+    "Equipment",
+    "Materials",
+    "Monsters",
+    "Treasure",
+  ];
 
   useEffect(async () => {
     try {
       setLoading(true);
       //category = "Entry" or ["Creatures", "Equipment", "Materials", "Monsters", "Treasure"]
       const response = await fetch(
-        `https://botw-compendium.herokuapp.com/api/v3/compendium/${category}/${id}`
+        `https://botw-compendium.herokuapp.com/api/v3/compendium/${category.toLowercase()}/${entryId.toLowercase()}`
       );
       const result = response.json();
       console.log(result);
@@ -23,13 +35,15 @@ function App() {
     } finally {
       setLoading(false);
     }
-  }, []);
+  }, [category, entryId]);
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/" element={<Navigationbar />}>
+            <Route path="index" element={<Homepage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
