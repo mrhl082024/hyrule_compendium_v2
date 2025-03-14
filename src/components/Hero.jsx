d9945283f0ce669dd1e19fa42b7254907f8110c2
mrhl082024 <mrhl082024@gmail.com>
2025-03-14 11:45:44 +0100

    ADD Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,15 @@ function Hero() {
       <div id="hero-card">
         <section id="hero-text">
           <h2> Welcome to Hyrule_Compendium_v2!</h2>
+          <br />
           <p>
             My goals for this project was to learn more about prop drilling,
             condtional statements, useState, browserouter, how to cache data,
-            Api calls and data structure. I'm very happy with what i've been
-            able to create, although it's mostly text and images so nothing
+            Api calls and data structure. <br /> I'm very happy with what i've
+            been able to create, although it's mostly text and images so nothing
             fancy, it's still a proof to myself that i'm aquiring new knowlegde
             and improving upon my existing ones :3
           </p>
-          <p>This was made using React.</p>
         </section>
       </div>
     </>
